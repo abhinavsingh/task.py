fcc06b7121d63217397d443aea3fe07abc460f84
Abhinav Singh <mailsforabhinav@gmail.com>
2013-12-12 23:36:01 +0530

    update simple_task to demonstrate Task.stop() API

--- examples/simple_task.py
+++ examples/simple_task.py
@@ -4,14 +4,16 @@ from task import Task
 def simple_task(t):
     assert t.args[0] == "hello"
     assert t.kwargs['world'] == "world"
-    time.sleep(1) # do some work
+    while not t.stopped():
+        time.sleep(0.1) # do some work
     return "done."
 
 if __name__ == "__main__":
     t = Task(simple_task, "hello", world="world")
     t.start()
     
-    while not t.done:
-        time.sleep(0.5)
+    # after 1 second signal our task to stop
+    time.sleep(1)
+    t.stop()
     
     print t.result
